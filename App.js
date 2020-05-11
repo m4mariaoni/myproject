b66eabad06825c692058ea90bdf984ec12ebf66f
Maria Simeon <msimeon@inlaks.com>
2020-05-11 23:48:19 +0100

    changes

--- App.js
+++ App.js
@@ -1,19 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Header from "./components/header";
+import ToDoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        console.log("ttt", text);
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS!", "todos must be over 3 chars long", [
+        {
+          text: "Understood",
+          onPress: () => {
+            console.log("alert closed");
+          },
+        },
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    // <Sandbox />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        console.log("dismiss keyboard");
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <ToDoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // paddingTop: 40,
+    // paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
+  item: {
+    marginTop: 24,
+    padding: 30,
+   
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
+  },
+  content: {
+    padding: 40,
+   
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+   
+    flex:1
+  },
+  // input: {
+  //   borderWidth: 1,
+  //   borderColor: "#777",
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200,
+  // },
 });
